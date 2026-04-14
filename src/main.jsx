a999ff6a783a105b4018c10e08f9a4ee5464df0a
PinedaAgudelo2003 <brahianpineda12@gmail.com>
2026-04-13 19:11:02 -0500

    modificacion

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
-import './styles/global.css';
+import './styles/global.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ReactDOM from 'react-dom/client'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taller2/'
+  base: '/Taller-2-refuerzo/'
 })
