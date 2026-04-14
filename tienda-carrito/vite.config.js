0bbe02cf6be063339e46aa84909ebd170f9fdb15
PinedaAgudelo2003 <brahianpineda12@gmail.com>
2026-04-13 19:46:58 -0500

    modificacion

--- tienda-carrito/vite.config.js
+++ tienda-carrito/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE || '/'
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base
 })
