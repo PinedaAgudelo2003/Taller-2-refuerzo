b13ceb6cf8bf2839c02c0267e731ad893ed71fc6
PinedaAgudelo2003 <brahianpineda12@gmail.com>
2026-04-13 20:31:40 -0500

    modificacion gitpages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE || '/Taller-2-refuerzo/'
+// const base = process.env.VITE_BASE || '/Taller-2-refuerzo/'
 
 export default defineConfig({
   plugins: [react()],
-  base
+  base: '/Taller-2-refuerzo/'
 })
